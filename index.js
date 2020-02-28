7a8aa0a06bc3249814aec27c5e4991c6baaaeefb
Anh Vuong <vuonga1103@yahoo.com>
2020-02-28 16:01:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // add solution here
+var theBeatlesPlay = (arrayMusician, arrayInstrument) => {
+  //returns an array of strings containing what instruments each musician plays
+  let newArray = [];
+  for (let x = 0; x < arrayMusician.length; x++) {
+    let musician = arrayMusician[x];
+    let instrument = arrayInstrument[x];
+    newArray.push(`${musician} plays ${instrument}`);
+  }
+  return newArray;
+}
+
+var johnLennonFacts = arrayFacts => {
+  let x = 0;
+  let newArr = [];
+  while (x < arrayFacts.length) {
+    newArr.push(`${arrayFacts[x]}!!!`);
+    x++;
+  }
+  return newArr;
+  //use a while loop to loop over the array and add "!!!" to the end of every fact.
+  //return an array of strings with exclamation points
+}
+
+var iLoveTheBeatles = num => {
+  let arr = [];
+
+  do {
+    arr.push('I love the Beatles!');
+    num++;
+  } 
+  while (num < 15);
+
+  return arr;
+}
+
